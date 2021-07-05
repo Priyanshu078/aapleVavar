8a7802ee592a7a2ba953f17db850778c231981af
Priyanshu Paliwal <66347715+Priyanshu078@users.noreply.github.com>
2021-07-06 00:04:38 +0530

    initializing Firebase App

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'SplashScreen.dart';
 import 'login/loginmain.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     theme: ThemeData(primaryColor: Colors.orange),
     darkTheme: ThemeData(primaryColor: Colors.orange),
